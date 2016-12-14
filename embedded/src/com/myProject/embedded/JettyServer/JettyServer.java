0ae51877608b6ac1bb0e963564862020eff6d3b6
Yang <y655li@uwaterloo.ca>
2016-12-13 22:29:25 -0500

    init commit, server can boot

---
+++ embedded/src/com/myProject/embedded/JettyServer/JettyServer.java
@@ -0,0 +1,42 @@
+package com.myProject.embedded.JettyServer;
+
+/**
+ * Created by yang on 13/12/16.
+ */
+
+
+
+import org.eclipse.jetty.server.Server;
+//import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+
+public class JettyServer {
+
+    public static void main(String[] args) {
+        try {
+            int port = 8080;
+            Server server = new Server(port);
+            WebAppContext context = new WebAppContext();
+            String dir = System.getProperty("user.dir");
+            dir = dir + "/embedded/web/WEB-INF";
+            System.out.println(dir);
+            System.out.println("ASdsada");
+
+            context.setResourceBase(dir);
+            context.setContextPath("/yang");
+
+//            context.setParentLoaderPriority(true);
+            server.setHandler(context);
+
+            server.start();
+            server.join();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
